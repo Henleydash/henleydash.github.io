428b573fa487b684861c2460f671652e563d26b1
dao116105-a11y <dao116105@gmail.com>
2026-03-12 18:02:11 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Exemple : Animation simple au défilement
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-});
-
-console.log("Bienvenue sur la plateforme Tech !");
